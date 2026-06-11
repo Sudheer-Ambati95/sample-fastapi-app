8136124cf4f42e3597a7c5f7b33f83bb7be788bd
Sudheer-Ambati95 <sudheerbaba.ambati@gmail.com>
2026-06-11 12:29:25 +0530

    fix(ci): resolve flake8 and pytest issues

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "FastAPI running successfully"}
+
 
 @app.get("/health")
 def health_check():
